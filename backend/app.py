e4b512ead4913e34812e6789756dd0a82edf3a10
Suneethalokarapu <suneethalokarapu@gmail.com>
2026-06-28 19:56:38 +0530

    Add React frontend UI for house price prediction

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import joblib
 import numpy as np
 
@@ -12,30 +12,13 @@ def home():
     return "Model Loaded Successfully"
 
 @app.route("/predict")
+@app.route("/predict", methods=["POST"])
 def predict():
 
-    sample_data = np.array([[
-        -122.23,
-        37.88,
-        41.0,
-        880.0,
-        129.0,
-        322.0,
-        126.0,
-        8.3252,
-        0,
-        0,
-        1,
-        0
-    ]])
-
-    scaled_data = scaler.transform(sample_data)
-
-    prediction = model.predict(scaled_data)
+    data = request.get_json()
 
     return jsonify({
-        "predicted_price": float(prediction[0])
+        "you_sent": data
     })
-
 if __name__ == "__main__":
     app.run(debug=True)
